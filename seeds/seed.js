c32f1e5c3ba47c26407f44e3e55df5266d65497c
CHAD GANDELMAN <cdgandelman@yahoo.com>
2023-04-06 17:34:09 -0600

    changes

---
+++ seeds/seed.js
@@ -0,0 +1,25 @@
+const sequelize = require('../config/connection');
+const { User, Trip } = require('../models');
+
+const userData = require('./userData.json');
+const tripData = require('./tripData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const trip of tripData) {
+    await Trip.create({
+      ...trip,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  process.exit(0);
+};
+
+seedDatabase();
